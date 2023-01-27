4f9dec7e32d9e25c2c77f2ffdc7adc013d066985
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-01-27 05:01:21 +0000

    msm8953-common: nuke some libhidl
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -222,9 +222,7 @@ endif
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport \
